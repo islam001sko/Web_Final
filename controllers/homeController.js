1941589738a3fef383495a4067d3c6a01dd646c9
islam001sko <zhaslanuly01@gmail.com>
2024-02-28 01:50:00 +0600

    b

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,7 @@
 const getHomePage = async (req, res, next) => {
     const isLoggedIn = req.session.user ? true : false;
-    const user = req.session.user; 
+    const user = req.session.user ? req.session.user : {}; 
     req.session.save();
-
     res.render('home', {
         user: user,
         isLoggedIn: isLoggedIn
